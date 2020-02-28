b9a9c6649133e64d416794cc0ef8cdaa5928fab8
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-02-28 07:16:54 -0800

    header file

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <p>Best Community Services</p>
+            <img 
+                src="BestComServices.png" 
+                alt="CompanyLogo?"
+            />
+            
+        </header>
+    )
+}
+
+export default Header
