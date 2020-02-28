31bde5b14ba61d83fcddd0db8096a4657545a7f3
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-02-27 22:21:19 -0800

    "vxcvxcv"

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Styles.css';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="Banner">
+                <header className="Home-header">
+                    BEST COMMUNITY SERVICES
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Home;
