2e3d576c6d0a33d74abd5b3e089c4a1d2fe7d334
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-03-02 13:04:47 -0800

    Create Volunteer.js

---
+++ src/Volunteer.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+
+/*
+This class displays the Volunteer's main page.
+*/
+class Volunteer extends Component {
+    constructor() {
+        super()
+        let pageId = 3;
+    }
+    
+    /*
+    This function displays the current services available
+    */
+    function currentServices() {
+      return (
+      
+      )
+    }
+    
+    /*
+    This function lists the upcoming services the
+    current volunteer is enlisted for
+    */
+    function enlistedServices() {
+      return (
+      
+      )
+    }
+    
+    render() {
+        return (
+            <div>
+                    <img src="calendar.png"/>
+                    <h2>Welcome Volunteer!</h2>
+                    <currentServices />
+                    <enlistedServices />
+            </div>
+        )
+    }
+}
+
+export default Volunteer
