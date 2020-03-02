e617042ad81a647dfd7959de801a243fc053ad29
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-03-02 12:20:44 -0800

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,27 @@ class Home extends Component {
         let pageId = 0;
     }
     
+    /*
+    This function attempts to connect to the user database
+    and validates 
+    */
+    function login() {
+        let user = this.props.userName;
+        let password = this.props.password;
+        var current = 0;
+        while (userdatabase[current] != NULL) {
+            if(user === current.userName  && password === current.password) {
+                //Check if user is volunter or employee
+                
+            }
+            current++;
+        }
+        <h2>Login Attempt Failed</h2>
+    }
     render() {
         return (
             <div>
-                    <button>Login</button>
+                    <button onClick="login()">Login</button>
                     <button>Donate</button>
                     <h2>Mission Statment</h2>
             </div>
