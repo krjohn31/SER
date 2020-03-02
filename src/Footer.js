578045cdac72bb8e1648e54a0b2473a8cb472587
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-03-02 07:52:26 -0800

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+/*
+This file contains and displays the contact information.
+*/
+function Footer() {
+    return (
+        <footer>
+            <p>Address: 17106 Tahoe Blvd Santa Cruz, CA</p>
+            <p>Phone: 1-800-509-1010</p>
+            />
+        </footer>
+    )
+}
+
+export default Footer
