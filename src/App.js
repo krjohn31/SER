a673ebbfb34c1a4627a46569006652b3c25d4633
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-03-02 07:56:53 -0800

    Add Footer and comments.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import Footer from './Footer';
 
+/*
+This file executes the display starting with the Home page.
+*/
 function App() {
   return (
     <div>
         <Header />
         <Home /> 
+        <Footer />
     </div>
   );
 }
