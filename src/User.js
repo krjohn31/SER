376a612c22b294d61212b3a4f257e238f2190ca2
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-03-02 13:02:32 -0800

    Create User.js

---
+++ src/User.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+
+/*
+This class displays the User's main page.
+*/
+class Home extends Component {
+    constructor() {
+        super()
+        let pageId = 2;
+    }
+    
+    /*
+    This function displays the current services available
+    */
+    function currentServices() {
+      return (
+      
+      )
+    }
+    
+    /*
+    This function lists the upcoming services the
+    current user has requested
+    */
+    function upcomingServices() {
+      return (
+      
+      )
+    }
+    
+    render() {
+        return (
+            <div>
+                    <img src="calendar.png"/>
+                    <h2>Welcome User!</h2>
+                    <currentServices />
+                    <upcomingServices />
+            </div>
+        )
+    }
+}
+
+export default User
