7d5928014c733d541f76cd7982d4738f3b9a740b
krjohn31 <32437627+krjohn31@users.noreply.github.com>
2020-03-03 10:17:38 -0800

    Update Employee.js

--- src/Employee.js
+++ src/Employee.js
@@ -14,8 +14,15 @@ class Employee extends Component {
     and their status
     */
     function currentServices() {
+        // Connect with services database
+        var services[] = servicesSQL.getTable();
       return (
-      
+        // Iterate through services table and display them
+        for(var i=0; services[i]!=NULL; i++) {
+            System.out.print.services[i].name;
+            System.out.print.services[i].description;
+            System.out.print.services[i].date;
+        }
       )
     }
     
